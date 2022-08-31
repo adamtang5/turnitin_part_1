be15f78f7882136b1193725b0e0ad8c1fbb325f6
Adam Tang <adamtang@gmail.com>
2022-08-31 09:59:18 -0700

    wrote js code for delay(ms)

--- 1-promise-delay.js
+++ 1-promise-delay.js
@@ -1,4 +1,9 @@
 // ms: number of milliseconds
 // returns a Promise that is resolved after ms milliseconds
 function delay(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
 }
