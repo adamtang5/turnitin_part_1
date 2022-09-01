1c20bf36dd4ea608898c6579dbb6e4c3bcff3537
Adam Tang <adamtang@gmail.com>
2022-09-01 09:39:33 -0700

    included clarification note

--- 03-remove-duplicates.js
+++ 03-remove-duplicates.js
@@ -1,4 +1,16 @@
 // xs: array
 // returns: a new array, with unique items
+
+/*
+Upon clarification:
+Someone on the tech team had this response to your question:
+I would say this exercise is referring to value equality only.
+I would focus this function on data types passed by value and
+omit those passed by reference.
+*/
+
 function removeDuplicates(xs) {
+    const set = new Set();
+    xs.forEach(el => set.add(el));
+    return Array.from(set);
 }
