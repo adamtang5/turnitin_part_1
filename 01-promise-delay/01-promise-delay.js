b50b8a560b87cc8643826e0591d719c2ccd51acb
Adam Tang <adamtang@gmail.com>
2022-09-02 17:27:45 -0700

    started writing test for 01-promise-delay

--- 01-promise-delay/01-promise-delay.js
+++ 01-promise-delay/01-promise-delay.js
@@ -1,9 +1,9 @@
+const makePromise = require('./test/promise-delay');
+
 // ms: number of milliseconds
 // returns a Promise that is resolved after ms milliseconds
 function delay(ms) {
     return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, ms);
+        setTimeout(() => resolve(), ms);
     });
 }
